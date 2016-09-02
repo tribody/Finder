489a5325de3df8090d815fcaf9d579bea85f015c
riverhe <125808587@qq.com>
2016-09-02 16:42:14 +0800

    modify from tribody

--- js/my-app.js
+++ js/my-app.js
@@ -1,9 +1,20 @@
 // Initialize app and store it to myApp variable for futher access to its methods
-              var myApp = new Framework7();
+              var myApp = new Framework7({
+                swipePanel: 'left'
+              });
 
               // We need to use custom DOM library, let's save it to $$ variable:
               var $$ = Dom7;
 
+              //to get the swiper
+              var mySwiper = myApp.swiper('.swiper-container', {
+                pagination: '.swiper-pagination',
+                paginationHide: false,
+                paginationClickable: true,
+                nextButton: '.swiper-button-next',
+                prevButton: '.swiper-button-prev',
+              });    
+              
               // Add view
               var mainView = myApp.addView('.view-main', {
                 // Because we want to use dynamic navbar, we need to enable it for this view:
@@ -46,3 +57,5 @@
                   });
                 });
             });
+
+
