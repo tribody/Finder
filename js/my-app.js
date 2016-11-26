c85481920892a063d94b8d38cc0331d23254a018
RiverHe <hctongxue@163.com>
2016-11-27 00:30:29 +0800

    change homepage

--- js/my-app.js
+++ js/my-app.js
@@ -1,6 +1,8 @@
 // Initialize app and store it to myApp variable for futher access to its methods
 var myApp = new Framework7({
-
+    pushstate: true,
+    swipePanel: 'left',
+    modalTitle: 'Finder'
 });
 
 // We need to use custom DOM library, let's save it to $$ variable:
@@ -34,26 +36,6 @@ var mainView = myApp.addView('.view-main', {
   dynamicNavbar: true
 });
 
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-  // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-  // Get page data from event data
-  var page = e.detail.page;
-
-  if (page.name === 'about') {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-  }
-})
-
-// Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="upload"]', function (e) {
   // Following code will be executed for page with data-page attribute equal to "about"
   // uploader
